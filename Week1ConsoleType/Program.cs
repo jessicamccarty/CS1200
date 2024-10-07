4a87ed55a05f0af6453fbb2981c8cf93c926cabf
Jessica McCarty <jessicaeitr@gmail.com>
2024-10-07 10:53:24 -0500

    ConsoleType

---
+++ Week1ConsoleType/Program.cs
@@ -0,0 +1,27 @@
+﻿// Write a message without a new line at the end
+Console.Write("Enter your name: ");
+
+// Read the user's name and store it in a variable
+string name = Console.ReadLine();
+
+// Greet the user with their name
+Console.WriteLine("Hello," + name + "!");
+
+// Prompt the user to press a key to continue
+Console.WriteLine("Press any key to continue...");
+
+// Wait for the user to press a key
+Console.ReadKey();
+
+// Clear the contents of the console window
+Console.Clear();
+
+// Demonstrate the Read() Method
+Console.WriteLine("\nEnter a single character: ");
+
+// Read a single character input from the user
+int inputChar = Console.Read();
+
+// Display the character as a number (ASCII value)
+Console.WriteLine("ASCII value: " + inputChar);
+
