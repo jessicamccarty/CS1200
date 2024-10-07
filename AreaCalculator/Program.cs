afa1bf8cbd8b4178e243798b26bd02446e7f2257
Jessica McCarty <jessicaeitr@gmail.com>
2024-10-07 17:39:37 -0500

    Area Calculator Incomplete

---
+++ AreaCalculator/Program.cs
@@ -0,0 +1,19 @@
+﻿// Author: Jessica McCarty
+// Date: October 7, 2024
+// Requirements: Prompt the user for the type of shape they want to use (rectangle, triangle, circle), then prompt them for the necessary values, calculate the area, and output
+// the result. All values should be rounded to two decimal places and trailing zeros should be output. You will need the value for Pi, availabe in the Math type. You can use the 
+// double type for all values. Use a switch statement as the primary conditional structure and display a message for invalid input.
+// Math.Round() and Math.Pi
+
+
+// User input for type of shape
+
+// For rectangle, user input for length
+
+// For rectangle, user input for width
+
+// For triangle, user input for base
+
+// For triangle, user input for height
+
+// For circle, user input for radius
