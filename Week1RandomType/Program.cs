563b52e5fca92a93a558ce8f6057a3fbc77d4294
Jessica McCarty <jessicaeitr@gmail.com>
2024-10-07 16:54:10 -0500

    RandomType

---
+++ Week1RandomType/Program.cs
@@ -0,0 +1,26 @@
+﻿Random rand = new Random();
+
+// int randomNumber = rand.Next(1, 6);
+// Console.WriteLine(randomNumber);
+
+// Generate 2 random numbers
+int dice1 = rand.Next(1,6);
+int dice2 = rand.Next(1,6);
+int sum = dice1 + dice2;
+
+// If loop
+
+if (dice1 == 1 && dice2 == 1) {
+    Console.WriteLine($"You rolled {dice1}, {dice2}.\nSnake Eyes!");
+} else if (dice1 == 6 && dice2 == 6) {
+    Console.WriteLine($"You rolled {dice1}, {dice2}.\nBox Cars!");
+} else {
+    Console.WriteLine($"You rolled {dice1}, {dice2}.\n{sum}");
+}
+
+// if(randomNumber % 2 == 0) {
+//     Console.WriteLine($"{randomNumber} is even.");
+// } else {
+//     Console.WriteLine($"{randomNumber} is odd.");
+// }
+
