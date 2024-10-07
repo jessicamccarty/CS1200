f4fc1fa9b0a93d36861011bdf8e9f349a6f6b3c7
Jessica McCarty <jessicaeitr@gmail.com>
2024-10-07 14:31:55 -0500

    Parsing

---
+++ Week1ParsingConsoleInput/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter an integer: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.Write("Enter a decimal: ");
+decimal num2 = decimal.Parse(Console.ReadLine());
+
+Console.Write("Enter a boolean (true/false): ");
+bool val = bool.Parse(Console.ReadLine());
+
+Console.WriteLine("You've entered " + num1 + ", " + num2 + ", and " + val);
