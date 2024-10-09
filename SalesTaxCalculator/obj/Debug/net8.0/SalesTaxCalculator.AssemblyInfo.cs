d7866458063760504959b019b339ed2c04186340
jessicamccarty <jessicaeitr@gmail.com>
2024-10-09 11:44:32 -0500

    Updated files per grader.

--- SalesTaxCalculator/obj/Debug/net8.0/SalesTaxCalculator.AssemblyInfo.cs
+++ SalesTaxCalculator/obj/Debug/net8.0/SalesTaxCalculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesTaxCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0270b1edf441e653b779260dc2afdf63bc38c540")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ac66acb3cef464241f808626f936e8335f7169f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesTaxCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesTaxCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
