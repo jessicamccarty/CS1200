c0812bce5a7d041eede3737e8bf196877bd8396d
jessicamccarty <jessicaeitr@gmail.com>
2024-10-15 12:06:37 -0500

    Updating files

--- ClassNotes/Program.cs
+++ ClassNotes/Program.cs
@@ -22,4 +22,43 @@ class Hello {
 // Ex. Convert to uppercase or lowercase before switch statement.
 
 
+// STRINGS, ARRAYS, LOOPS 10/15/2024
+// learn.microsoft.com
+
+string name = "alice";
+Console.WriteLine(name);
+name.ToUpper();
+Console.WriteLine(name); // Will print original string
+
+string capName = name.ToUpper(); // Give strring it's own variable name
+Console.WriteLine(capName);  // Show modified strring variable
+
+
+// This prints Hello. It assigned str1 as str2. We made a modification but never gave it a name to print. 
+string str1 = "Hello";
+string str2 = str1;
+str1 += "World";
+Console.WriteLine(str2);
+
+// This is how you would appropriately concatenate the strings.
+string str1 = "A string is more";
+string str2 = "than the sum of its chars.";
+str1 += str2; // The original str1 is now str1 with str2 concatenated.
+Console.WriteLine(s1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Array of names
+string[] names = { "Alice", "Bob", "Charlie", "David", "Eve"}
 
---
+++ Encounter/Program.cs
@@ -0,0 +1,30 @@
+﻿// Partners: Jessica McCarty and Remington
+
+// Character Variables
+int characterHP = 100;
+int characterAttack = 20;
+int characterDefense = 10;
+
+// Enemy Variables
+string[] enemyName = {"enemyOne", "enemyTwo", "enemyThree"};
+int[] enemyHP = {30, 40, 50};
+int[] enemyDefense = {5, 7, 10};
+int[] enemyAttack = {10, 15, 20};
+
+// Enemy Encounter Loop
+int i = 0;
+
+while (characterHP > 0 && enemyHP[i] > 0) {
+
+    for (i = 0; i < 3; i++) {
+        Console.WriteLine("You are walking across the planet and encounter an enemy...");
+
+        while (enemyHP[i] > 0) {
+            Console.WriteLine($"{enemyName[i]} with a health of {enemyHP[i]} has attacked you for {enemyAttack[i]}.");
+            Console.WriteLine($"You defend against {enemyName[i]}'s attack for {characterDefense} leaving your health at {characterHP - enemyAttack[i] + characterDefense}.");
+            Console.WriteLine($"Your counter attack against {enemyName[i]} for {characterAttack} was blocked for {enemyDefense[i]} leaving {enemyName[i]} with {enemyHP[i] - characterAttack + enemyDefense[i]} HP.");
+
+        } Console.WriteLine($"{enemyName[i]} has been defeated.");
+
+    }
+}
