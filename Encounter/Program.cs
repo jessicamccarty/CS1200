082cb0662ec6a3ee99423389d802d437be2a0575
Jessica McCarty <jessicaeitr@gmail.com>
2024-10-15 16:21:49 -0500

    Updated all files.

--- Encounter/Program.cs
+++ Encounter/Program.cs
@@ -11,20 +11,50 @@ int[] enemyHP = {30, 40, 50};
 int[] enemyDefense = {5, 7, 10};
 int[] enemyAttack = {10, 15, 20};
 
+// // Enemy Encounter Loop
+
+// while (characterHP > 0 && enemyHP[i] > 0) {
+
+
+//     for (i = 0; i < 3; i++) {
+//         Console.WriteLine("You are walking across the planet and encounter an enemy...");
+
+//         while (enemyHP[i] > 0) {
+//             Console.WriteLine($"{enemyName[i]} with a health of {enemyHP[i]} has attacked you for {enemyAttack[i]}.");
+//             Console.WriteLine($"You defend against {enemyName[i]}'s attack for {characterDefense} leaving your health at {characterHP - enemyAttack[i] + characterDefense}.");
+//             Console.WriteLine($"Your counter attack against {enemyName[i]} for {characterAttack} was blocked for {enemyDefense[i]} leaving {enemyName[i]} with {enemyHP[i] - characterAttack + enemyDefense[i]} HP.");
+
+//         } Console.WriteLine($"{enemyName[i]} has been defeated.");
+
+//     }
+// }
+
 // Enemy Encounter Loop
-int i = 0;
+for (int i = 0; i < enemyName.Length; i++)
+{
+    Console.WriteLine("You are walking across the planet and encounter an enemy...");
 
-while (characterHP > 0 && enemyHP[i] > 0) {
+    while (characterHP > 0 && enemyHP[i] > 0)
+    {
+        Console.WriteLine($"{enemyName[i]} with a health of {enemyHP[i]} has attacked you for {enemyAttack[i]}.");
+        characterHP -= enemyAttack[i] - characterDefense;
+        Console.WriteLine($"You defend against {enemyName[i]}'s attack, leaving your health at {characterHP}.");
 
-    for (i = 0; i < 3; i++) {
-        Console.WriteLine("You are walking across the planet and encounter an enemy...");
+        // Check if character is defeated
+        if (characterHP <= 0)
+        {
+            Console.WriteLine("You have been defeated.");
+            break; // Exit the loop if defeated
+        }
 
-        while (enemyHP[i] > 0) {
-            Console.WriteLine($"{enemyName[i]} with a health of {enemyHP[i]} has attacked you for {enemyAttack[i]}.");
-            Console.WriteLine($"You defend against {enemyName[i]}'s attack for {characterDefense} leaving your health at {characterHP - enemyAttack[i] + characterDefense}.");
-            Console.WriteLine($"Your counter attack against {enemyName[i]} for {characterAttack} was blocked for {enemyDefense[i]} leaving {enemyName[i]} with {enemyHP[i] - characterAttack + enemyDefense[i]} HP.");
+        enemyHP[i] -= characterAttack - enemyDefense[i];
+        Console.WriteLine($"Your counter attack against {enemyName[i]} for {characterAttack} was blocked for {enemyDefense[i]}, leaving {enemyName[i]} with {enemyHP[i]} HP.");
 
-        } Console.WriteLine($"{enemyName[i]} has been defeated.");
-
+        // Check if enemy is defeated
+        if (enemyHP[i] <= 0)
+        {
+            Console.WriteLine($"{enemyName[i]} has been defeated.");
+            break; // Exit the loop if the enemy is defeated
+        }
     }
 }
--- HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fe3f34cce3e5b9fef8353c0257474a3cdc57b68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0812bce5a7d041eede3737e8bf196877bd8396d")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
