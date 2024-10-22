028c89c33a2a7dbd0e07bb34c458200fde78d01b
jessicamccarty <jessicaeitr@gmail.com>
2024-10-22 12:01:21 -0500

    updating files

--- ClassKnowledgeCheck/obj/Debug/net8.0/ClassKnowledgeCheck.AssemblyInfo.cs
+++ ClassKnowledgeCheck/obj/Debug/net8.0/ClassKnowledgeCheck.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassKnowledgeCheck")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fb3a76fecb0da651416cf3fbf5d22cf11f4f7df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bc1fff086a5df792d3a5ecf996eed0df06ef102")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassKnowledgeCheck")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassKnowledgeCheck")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TextRPG/obj/Debug/net8.0/TextRPG.AssemblyInfo.cs
+++ TextRPG/obj/Debug/net8.0/TextRPG.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TextRPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01dda8defb5dbeaebc29784838fdfd04c6a6465d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bc1fff086a5df792d3a5ecf996eed0df06ef102")]
 [assembly: System.Reflection.AssemblyProductAttribute("TextRPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TextRPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
