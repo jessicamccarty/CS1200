1387c7d05deaee7d9450bdaf0f13ff5e7e4717a7
jessicamccarty <jessicaeitr@gmail.com>
2024-10-23 19:25:02 -0500

    merge files

--- HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d5109d98e76b5aafe39cacc4e26cc7f46a89570")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ba75209f96aac564f788db78741c4fd31f373eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
